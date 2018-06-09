63d6d9c347433b77a0328dfcf32da9715355f28c
十一郎love <zhb1316783262@163.com>
2018-06-09 15:30:19 +0800

    完善

--- my-cloud-parent/my-cloud-sleuth/src/main/java/com/zhb/cloud/MyCloudSleuthApplication.java
+++ my-cloud-parent/my-cloud-sleuth/src/main/java/com/zhb/cloud/MyCloudSleuthApplication.java
@@ -2,12 +2,10 @@ package com.zhb.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
 @SpringBootApplication
 @EnableZipkinStreamServer
-@EnableDiscoveryClient
 public class MyCloudSleuthApplication {
 
 	public static void main(String[] args) {
